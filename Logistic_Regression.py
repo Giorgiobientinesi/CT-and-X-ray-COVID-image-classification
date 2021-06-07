9915ccc50bd667bf7e1378855f616f9818c3fe9c
Giorgio Bientinesi <77893973+Giorgiobientinesi@users.noreply.github.com>
2021-06-07 23:05:30 +0200

    Add files via upload

---
+++ Logistic_Regression.py
@@ -0,0 +1,96 @@
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import pickle
+import random
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingGridSearchCV  #!pip install scikit-learn==0.24.2
+
+path = "CT/COVID"   #Same for X-ray
+
+
+data = []
+
+i = 0
+while i < len(os.listdir(path)):
+  try:
+    path1 = os.path.join(path, os.listdir(path)[i])
+    SP500_img = cv2.imread(path1,0)
+    SP500_img = cv2.resize(SP500_img, (150,150))
+    image = np.array(SP500_img).flatten()
+    data.append([image,0])
+  except Exception:
+    print("errore")
+  i+=1
+
+path = "CT/Non-COVID"
+
+i = 0
+while i < len(os.listdir(path)):
+  try:
+    path1 = os.path.join(path, os.listdir(path)[i])
+    SP500_img = cv2.imread(path1,0)
+    SP500_img = cv2.resize(SP500_img, (150,150))
+    image = np.array(SP500_img).flatten()
+    data.append([image,1])
+  except Exception:
+    print("errore")
+  i+=1
+
+random.shuffle(data)
+
+Features = []
+Labels = []
+
+for features,label in data:
+    Features.append(features)
+    Labels.append(label)
+
+X_train, X_test, y_train, y_test = train_test_split(Features, Labels, test_size = 0.2)
+
+
+from sklearn.model_selection import RandomizedSearchCV
+C = [1, 5, 10,20]
+penalty = ["l1", "l2"]
+solver = ["liblinear", "saga"]
+
+# Create the random grid
+random_grid = {'C': C,
+               'penalty': penalty,
+               'solver': solver}
+print(random_grid)
+
+model = LogisticRegression() #Run the model and find the best structure
+grid_search = HalvingGridSearchCV(model, random_grid, random_state=0, verbose=1) #cv default = 5
+grid_search.fit(X_train,y_train)
+
+# Found the best
+
+classifier = LogisticRegression(C=1, penalty="l2", solver="liblinear")
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score,recall_score,precision_score,f1_score
+print('Accuracy Score : ' + str(accuracy_score(y_test,y_pred)))
+print('Precision Score : ' + str(precision_score(y_test,y_pred)))
+print('Recall Score : ' + str(recall_score(y_test,y_pred)))
+print('F1 Score : ' + str(f1_score(y_test,y_pred)))
+
+from sklearn.metrics import confusion_matrix
+print('Confusion Matrix : \n' + str(confusion_matrix(y_test,y_pred)))
+
+
+y_pred1 = classifier.predict(X_train)
+
+print('Accuracy Score : ' + str(accuracy_score(y_train,y_pred1)))
+print('Precision Score : ' + str(precision_score(y_train,y_pred1)))
+print('Recall Score : ' + str(recall_score(y_train,y_pred1)))
+print('F1 Score : ' + str(f1_score(y_train,y_pred1)))
+
+print('Confusion Matrix : \n' + str(confusion_matrix(y_train,y_pred1)))
---
+++ SVM.py
@@ -0,0 +1,90 @@
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import pickle
+import random
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_validate
+from sklearn.experimental import enable_halving_search_cv  #!pip install scikit-learn==0.24.2
+from sklearn.model_selection import HalvingGridSearchCV
+
+path = "CT/COVID"  #Same for X-ray
+
+
+data = []
+
+i = 0
+while i < len(os.listdir(path)):
+  try:
+    path1 = os.path.join(path, os.listdir(path)[i])
+    SP500_img = cv2.imread(path1,0)
+    SP500_img = cv2.resize(SP500_img, (150,150))
+    image = np.array(SP500_img).flatten()
+    data.append([image,0])
+  except Exception:
+    print("errore")
+  i+=1
+
+path = "CT/Non-COVID"
+
+i = 0
+while i < len(os.listdir(path)):
+  try:
+    path1 = os.path.join(path, os.listdir(path)[i])
+    SP500_img = cv2.imread(path1,0)
+    SP500_img = cv2.resize(SP500_img, (150,150))
+    image = np.array(SP500_img).flatten()
+    data.append([image,1])
+  except Exception:
+    print("errore")
+  i+=1
+
+random.shuffle(data)
+
+Features = []
+Labels = []
+
+for features,label in data:
+    Features.append(features)
+    Labels.append(label)
+
+X_train, X_test, y_train, y_test = train_test_split(Features, Labels, test_size = 0.2)
+
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                     'C': [1, 10, 100, 1000]},
+                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+model = SVC() #Run the model and find the best structure
+grid_search = HalvingGridSearchCV(model, tuned_parameters, scoring="accuracy",verbose=3) #cv default = 5
+grid_search.fit(X_train,y_train)
+
+#found the best
+
+classifier = SVC(C=1000, kernel="linear")
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score,recall_score,precision_score,f1_score
+print('Accuracy Score : ' + str(accuracy_score(y_test,y_pred)))
+print('Precision Score : ' + str(precision_score(y_test,y_pred)))
+print('Recall Score : ' + str(recall_score(y_test,y_pred)))
+print('F1 Score : ' + str(f1_score(y_test,y_pred)))
+
+from sklearn.metrics import confusion_matrix
+print('Confusion Matrix : \n' + str(confusion_matrix(y_test,y_pred)))
+
+y_pred1 = classifier.predict(X_train)
+
+print('Accuracy Score : ' + str(accuracy_score(y_train,y_pred1)))
+print('Precision Score : ' + str(precision_score(y_train,y_pred1)))
+print('Recall Score : ' + str(recall_score(y_train,y_pred1)))
+print('F1 Score : ' + str(f1_score(y_train,y_pred1)))
+
+print('Confusion Matrix : \n' + str(confusion_matrix(y_train,y_pred1)))
